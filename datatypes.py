c15ace99bf137c20a4219c28807e7a4f18f5339f
Sravank2003 <sravankumar18035@gmail.com>
2024-08-13 18:38:00 +0530

    py

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+var_int=4
+print(type(var_int))
+pavan=4.5
+print(type(pavan))
+shwetha=4j
+print(type(4j))
+kishore="data"
+print(type(kishore))
+manu=["data engineer", "data analyst"]
+print(type(manu))
+vinit=("data engineer", "data analyst")
+print(type(vinit))
+nagasai={"name":"age", "key":"value"}
+print(type(nagasai))
